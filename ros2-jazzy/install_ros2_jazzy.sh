77bd72acfb5016f0abe4f355a48b22a15a4c3e7e
Heiko Schoon <135117019+hschoon-dev@users.noreply.github.com>
2025-06-18 16:01:43 +0200

    Update install_ros2_jazzy.sh

--- ros2-jazzy/install_ros2_jazzy.sh
+++ ros2-jazzy/install_ros2_jazzy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# general setup
 sudo apt update && sudo apt upgrade
-sudo apt install build-essentials
+sudo apt install build-essential gcc make perl dkms
+
+# WHEN USING VBOX: then install Guest-Addition-CD, sudo ./VBoxLinuxAdditions.run
